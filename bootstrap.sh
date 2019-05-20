b5dc58b06b6668b9bef54b35d1bd26ed99a83648
Kyle Tilman <kyletilman@gmail.com>
2019-05-20 19:22:58 -0400

    Iterm

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".macos" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		--exclude "Brewfile" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
-}
+rsync --exclude ".git/" \
+	--exclude ".DS_Store" \
+	--exclude ".macos" \
+	--exclude "bootstrap.sh" \
+	--exclude "README.md" \
+	--exclude "LICENSE-MIT.txt" \
+	--exclude "Brewfile" \
+	-avh --no-perms . ~;
 
-doit
 ./.macos
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew Brewfile
