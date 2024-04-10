f45e68a381161fa6d2786cefff99b3e4477c3d2e
kyle tilman <kyletilman@gmail.com>
2024-04-10 16:40:47 -0600

    more

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# ./.macos
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew bundle
+nvm install stable && nvm use stable
+npm i -g "$(cat global_npm)"
+pip3 install -r global_pip
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 rsync --exclude ".git/" \
 	--exclude ".DS_Store" \
 	--exclude ".macos" \
@@ -9,13 +17,6 @@ rsync --exclude ".git/" \
 	--exclude "Brewfile" \
 	-avh --no-perms . ~;
 
-# ./.macos
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew bundle
-nvm install stable && nvm use stable
-npm i -g $(cat global_npm)
-pip3 install -r global_pip
 nvim --headless +PlugInstall +qall
-nvim --headless +CocInstall coc-ultisnips coc-tsserver coc-css coc-json +qall
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
